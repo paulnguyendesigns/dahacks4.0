d398cb398acf127f4e3ffbd6088b12600a54e9ff
Alex <alexnguyen.adastra1261@gmail.com>
2025-11-22 15:44:45 -0800

    commit new celeb changes

--- frontend/src/CelebMatches.jsx
+++ frontend/src/CelebMatches.jsx
@@ -7,82 +7,52 @@ import "./index.css";
 */
 
 const SEASON_DATA = {
+  
   "Deep Autumn": {
     celebrities: [
       {
         name: "Zendaya",
-        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Feuphoria.fandom.com%2Fwiki%2FZendaya&psig=AOvVaw10eOqMJPQ5MDn6GEhxxVNf&ust=1763926609681000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCLDYx_LAhpEDFQAAAAAdAAAAABAE",
+        image: "./public/celebrityphotos/Z.PNG",
         outfits: [
           {
-            img: "https://cdn.shopify.com/s/files/1/0729/6214/6631/files/Neutral_Pastel_Aesthetic_Vintage_Moodboard_Photo_Collage_Grid_Instagram_Post_3_1_480x480.webp?v=1721735893",
-            desc: "Rust dress with deep teal earrings."
-          },
-          {
-            img: "https://i.pinimg.com/1200x/0b/7a/c6/0b7ac6ddd752e453192335a0fb00a7ed.jpg",
-            desc: "Olive green jacket with gold accessories."
+            img: "./public/celebrityphotos/dafemale.PNG",
+            desc: "Zendaya in reddish and brownish autumn hues."
           }
         ]
       },
       {
         name: "Jason Momoa",
-        image: "https://via.placeholder.com/200x250?text=Priyanka",
+        image: "./public/celebrityphotos/JM.PNG",
         outfits: [
           {
-            img: "https://via.placeholder.com/120?text=Terracotta",
-            desc: "Jason Momoa in his seasonal colors"
-          },
+            img: "./public/celebrityphotos/damale.PNG",
+            desc: "Jason Momoa in brownish autumn hues"
+          }
         ]
       }
     ]
   },
-  "Warm Autumn": {
+  
+  "True Autumn": {
     celebrities: [
       {
         name: "Julia Roberts",
-        image: "https://m.media-amazon.com/images/M/MV5BMTQzNjU3MDczN15BMl5BanBnXkFtZTYwNzY2Njc4._V1_FMjpg_UX1000_.jpg",
+        image: "./public/celebrityphotos/JR.PNG",
         outfits: [
           {
-            img: "https://cdn.shopify.com/s/files/1/0794/7776/8518/files/4_20b44500-ba26-41de-86ab-b78ee930d9f4.png?v=1757601573",
-            desc: "."
-          },
-          {
-            img: "https://imageconsultantmaidenhead.co.uk/cdn/shop/articles/2_82696ef8-5855-4f7e-a132-d59be6b0733a.png?v=1757601979",
-            desc: "Olive green jacket with gold accessories."
+            img: "./public/celebrityphotos/tafemale.PNG",
+            desc: "Auburn and Navy Blue suit."
           }
         ]
       },
       {
         name: "Bruno Mars",
-        image: "https://m.media-amazon.com/images/M/MV5BMjE1NDE3ODA0MV5BMl5BanBnXkFtZTcwODQ5NTgwNQ@@._V1_.jpg",
+        image: "./public/celebrityphotos/BB.PNG",
         outfits: [
           {
-            img: "https://thealignedlover.com/wp-content/uploads/2025/02/5-5-1024x576.jpg",
-            desc: "Jason Momoa in his seasonal colors"
-          },
-        ]
-      }
-    ]
-  },
-  "Soft Autumn": {
-    celebrities: [
-      {
-        name: "Gisele Bundchen ",
-        image: "https://cdn.shopify.com/s/files/1/0729/6214/6631/files/Minimal_Aesthetic_Story_Highlight_Covers_Your_Story_43_1_480x480.webp?v=1726932905",
-        outfits: [
-          {
-            img: "https://fourseasons.studio/cdn/shop/articles/Minimal_Aesthetic_Story_Highlight_Covers_Your_Story_42_1_1200x1200.webp?v=1726939083",
-            desc: ""
-          },
-        ]
-      },
-      {
-        name: "Ryan Reynolds",
-        image: "https://upload.wikimedia.org/wikipedia/commons/1/14/Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg",
-        outfits: [
-          {
-            img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.esquire.com%2Fstyle%2Fmens-fashion%2Fg20703481%2Fryan-reynolds-style-fashion-outfits%2F&psig=AOvVaw00BMy3cLDyO-Osnqtayu7R&ust=1763928294852000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCOCC9ZDHhpEDFQAAAAAdAAAAABAL",
-            desc: "."
-          },
+            img: "./public/celebrityphotos/tamale.PNG",
+            desc: "Mars in yellow, blue and red color combo."
+          }
         ]
       }
     ]
@@ -91,22 +61,22 @@ const SEASON_DATA = {
     celebrities: [
       {
         name: "Vanessa Hudgens",
-        image: "https://i.pinimg.com/474x/4f/c8/16/4fc816e1d46e6086cf589a4447c0c1f6.jpg",
+        image: "./public/celebrityphotos/VH.PNG",
         outfits: [
           {
-            img: "https://cdn.shopify.com/s/files/1/0729/6214/6631/files/Neutral_Pastel_Aesthetic_Vintage_Moodboard_Photo_Collage_Grid_Instagram_Post_22_480x480.webp?v=1721490158",
-            desc: "hd"
-          },
+            img: "./public/celebrityphotos/bsfemale.PNG",
+            desc: "Red and Pink spring gowns."
+          }
         ]
       },
       {
         name: "Zac Efron",
-        image: "https://cdn.britannica.com/33/215033-050-70D7B4A3/American-actor-Zac-Efron-2017.jpg",
+        image: "./public/celebrityphotos/ZE.PNG",
         outfits: [
           {
-            img: "https://thevou.com/wp-content/uploads/2024/08/Zac-Efron-Bright-Spring-colour-palette.jpg",
-            desc: "."
-          },
+            img: "./public/celebrityphotos/bsmale.PNG",
+            desc: "Zac Efron in bright spring red and blue ."
+          }
         ]
       }
     ]
@@ -114,52 +84,77 @@ const SEASON_DATA = {
   "Light Spring": {
     celebrities: [
       {
-        name: "Elsa Hosk ",
-        image: "https://58v76y8z87lo.hellomagazine.com/horizon/landscape/51d6a46ade9f-gettyimages-2244624405.jpg",
+        name: "Elsa Hosk",
+        image: "./public/celebrityphotos/EH.PNG",
         outfits: [
           {
-            img: "https://cdn.shopify.com/s/files/1/0729/6214/6631/files/Neutral_Pastel_Aesthetic_Vintage_Moodboard_Photo_Collage_Grid_Instagram_Post_18_deff22d8-673d-4539-aff1-3ce171e5277f_480x480.webp?v=1721314667",
-            desc: ""
-          },
+            img: "./public/celebrityphotos/lsfemale.PNG",
+            desc: "Soft pastel green and blue sets."
+          }
         ]
       },
       {
         name: "Chris Pine",
-        image: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Chris_Pine_%2843043463084%29_%28cropped%29.jpg",
+        image: "./public/celebrityphotos/CP.PNG",
         outfits: [
           {
-            img: "https://thevou.com/wp-content/uploads/2024/09/Chris-Pine-Light-Spring-Seasonal-Colour-Style.jpg",
-            desc: "."
-          },
-        ]
-      }
-    ]
-  },
-  "Warm Spring": {
-    celebrities: [
-      {
-        name: "Blake Lively ",
-        image: "https://m.media-amazon.com/images/M/MV5BMTU2Njk0NTUyNl5BMl5BanBnXkFtZTcwODE0OTgyNw@@._V1_.jpg",
-        outfits: [
-          {
-            img: "https://fourseasons.studio/cdn/shop/articles/Minimal_Aesthetic_Story_Highlight_Covers_Your_Story_-_2024-10-21T225359.828_1200x1200.webp?v=1729523503",
-            desc: ""
-          },
-        ]
-      },
-      {
-        name: "Pedro Pascal",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Pedro_Pascal_at_the_2025_Cannes_Film_Festival_05.jpg/960px-Pedro_Pascal_at_the_2025_Cannes_Film_Festival_05.jpg",
-        outfits: [
-          {
-            img: "https://media.gq.com/photos/63ff98d207f0feb01b2e1684/master/pass/pedro.jpg",
-            desc: "."
-          },
+            img: "./public/celebrityphotos/lsmale.PNG",
+            desc: "Peach and pastel rainbow hues."
+          }
         ]
       }
     ]
   },
 
+  "True Spring": {
+    celebrities: [
+      {
+        name: "Blake Lively",
+        image: "./public/celebrityphotos/BL.PNG",
+        outfits: [
+          {
+            img: "./public/celebrityphotos/tsfemale.PNG",
+            desc: "True orange and true blue suit."
+          }
+        ]
+      },
+      {
+        name: "Pedro Pascal",
+        image: "./public/celebrityphotos/PP.PNG",
+        outfits: [
+          {
+            img: "./public/celebrityphotos/tsmale.PNG",
+            desc: "Pedro Pascal in true spring yellow, brown and gray hues."
+          }
+        ]
+      }
+    ]
+  },
+
+  "Soft Autumn": {
+    celebrities: [
+      {
+        name: "Gisele Bundchen",
+        image: "./public/celebrityphotos/GB.PNG",
+        outfits: [
+          {
+            img: "./public/celebrityphotos/safemale.PNG",
+            desc: "Lavendar button-down and Cobalt blue suit."
+          }
+        ]
+      },
+      {
+        name: "Ryan Reynolds",
+        image: "./public/celebrityphotos/RR.PNG",
+        outfits: [
+          {
+            img: "./public/celebrityphotos/samale.PNG",
+            desc: "Hot pink dress for a crisp Winter pop."
+          }
+        ]
+      }
+    ]
+  },
   "True Winter": {
     celebrities: [
       {
@@ -283,19 +278,25 @@ const SEASON_DATA = {
     ]
   },
 
-  "Soft Summer": {
+  "Soft Summmer": {
     celebrities: [
       {
-        name: "Emma Watson",
-        image: "https://via.placeholder.com/200x250?text=Emma+Watson",
+        name: "Karisma Kapoor",
+        image: "./public/celebrityphotos/karismakapoor5064.PNG",
         outfits: [
           {
-            img: "https://via.placeholder.com/120?text=Lavender",
-            desc: "Soft lavender sweater that suits muted tones."
-          },
+            img: "./public/celebrityphotos/softsummerfemale.JPG",
+            desc: "Bright red crop top and drak blue gown."
+          }
+        ]
+      },
+      {
+        name: "Daniel Kim",
+        image: "./public/celebrityphotos/danielkim.PNG",
+        outfits: [
           {
-            img: "https://via.placeholder.com/120?text=Dusty+Blue",
-            desc: "Dusty blue top with minimal accessories."
+            img: "./public/celebrityphotos/softsummermale.PNG",
+            desc: "Blue and turquoise suit."
           }
         ]
       }
